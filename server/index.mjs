0fb019f7c71d81ad7c9bb2e5891f72f77641d913
Victor Gavro <vgavro@gmail.com>
2024-10-18 09:48:45 +0300

    feat(launchServer-options): more options to configure lauchServer from environment variables, fixes issue on providing custom port

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,6 @@
 import { chromium, firefox, webkit } from "playwright-core";
 import { existsSync } from "node:fs";
+import shellQuoteParse from "shell-quote/parse.js";
 
 main();
 
@@ -10,21 +11,40 @@ async function main() {
 
 async function launch() {
     const info = (process.env.IMAGE_INFO || "").toLowerCase();
-    const channel = process.env.BROWSER_CHANNEL;
-    const port = process.env.BROWSER_PORT || 53333;
-    const wsPath = process.env.BROWSER_WS_ENDPOINT || "/playwright";
+
+    // https://playwright.dev/docs/api/class-browsertype#browser-type-launch-server
+    const options = {
+        port: parseInt(process.env.BROWSER_PORT || 53333),
+        wsPath: process.env.BROWSER_WS_ENDPOINT || "/playwright",
+        channel: process.env.BROWSER_CHANNEL,
+    }
+    if (process.env.BROWSER_TIMEOUT) {
+        options.timeout = parseInt(process.env.BROWSER_TIMEOUT)
+    }
+    if (process.env.BROWSER_PROXY_SERVER) {
+        options.proxy = {
+            server: process.env.BROWSER_PROXY_SERVER,
+            bypass: process.env.BROWSER_PROXY_BYPASS,
+            username: process.env.BROWSER_PROXY_USERNAME,
+            password: process.env.BROWSER_PROXY_PASSWORD,
+        }
+    }
+    if (process.env.BROWSER_ARGS) {
+        options.args = shellQuoteParse(process.env.BROWSER_ARGS)
+    }
+
     if (info.includes("chromium")) {
         return existsSync("/etc/alpine-release")
-            ? await chromium.launchServer({ executablePath: "/usr/bin/chromium", port, wsPath })
-            : await chromium.launchServer({ port, wsPath });
+            ? await chromium.launchServer({ executablePath: "/usr/bin/chromium", ...options })
+            : await chromium.launchServer(options);
     } else if (info.includes("firefox")) {
-        return await firefox.launchServer({ port, wsPath });
+        return await firefox.launchServer(options);
     } else if (info.includes("webkit")) {
-        return await webkit.launchServer({ port, wsPath });
+        return await webkit.launchServer(options);
     } else if (info.includes("chrome")) {
-        return await chromium.launchServer({ executablePath: "/usr/bin/google-chrome", channel, port, wsPath });
+        return await chromium.launchServer({ executablePath: "/usr/bin/google-chrome", ...options });
     } else if (info.includes("msedge")) {
-        return await chromium.launchServer({ executablePath: "/usr/bin/microsoft-edge", channel, port, wsPath });
+        return await chromium.launchServer({ executablePath: "/usr/bin/microsoft-edge", ...options });
     } else {
         throw new Error(`Unknown browser info: ${info}`);
     }
